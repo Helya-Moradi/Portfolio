41cadc834c241c9c8dc73f55707ac619544a46da
Helya Moradi <helya.m78@gmail.com>
2024-04-07 13:46:19 +0330

    feat: add colors

--- src/sections/main_content/index.tsx
+++ src/sections/main_content/index.tsx
@@ -1,10 +1,12 @@
 import style from './index.sass'
 import Home from "./sections/home";
+import About from "src/sections/main_content/sections/about";
 
 function MainContent() {
     return (
         <div className={style.mainContent}>
             <Home/>
+            <About/>
         </div>
     );
 }
