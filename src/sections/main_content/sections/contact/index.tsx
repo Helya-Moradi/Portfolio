15218a22d26b16bb6728a795b51a6b9dc62c37a8
helya <helya.m78@gmail.com>
2024-04-10 19:26:39 +0330

    feat: contact section

--- src/sections/main_content/sections/contact/index.tsx
+++ src/sections/main_content/sections/contact/index.tsx
@@ -3,10 +3,46 @@ import style from "./index.sass"
 
 function Contact() {
     return (
-        // TODO: add children
-        <Template title='Contact' color='lightPurple' direction='ltr'>
+        <div className={style.contactContainer}>
+            <Template title='Contact' color='lightPurple' direction='ltr'>
+                <div className={style.contact}>
+                    <h3>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus fugit harum nihil
+                    </h3>
 
-        </Template>
+                    <p>
+                        Self-motivated and creative Web Designer and Front-end Developer. Interested in learning,
+                        programming, web design, artwork and problem solving.
+                    </p>
+
+                    <form className={style.contactForm}>
+                        <div className={style.inputWrapper}>
+                            <label>Name</label>
+                            <input type="text"/>
+                        </div>
+
+                        <div className={style.inputWrapper}>
+                            <label>Email</label>
+                            <input type="text"/>
+                        </div>
+
+                        <div className={style.inputWrapper}>
+                            <label>Subject</label>
+                            <input type="text"/>
+                        </div>
+
+                        <div className={style.inputWrapper}>
+                            <label>Message</label>
+                            <textarea/>
+                        </div>
+
+                        <button>
+                            Send Message
+                        </button>
+                    </form>
+                </div>
+            </Template>
+        </div>
     );
 }
 
