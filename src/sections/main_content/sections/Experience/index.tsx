f964c4704465327735ed3d2a39221877ecc892e5
helya <helya.m78@gmail.com>
2024-04-10 18:38:00 +0330

    feat: add text to education section

--- src/sections/main_content/sections/Experience/index.tsx
+++ src/sections/main_content/sections/Experience/index.tsx
@@ -7,13 +7,13 @@ function Experience() {
 
     const experienceItems = [
         {
-            title: 'Content preparation for Computer Student\'s Magazine (Magit)',
-            date: 'MAR 2021 - DEC 2021',
+            title: 'Member of Computer Scientific Student\'s Community',
+            date: 'DEC 2020 - DEC 2021',
             location: 'Ershad Damavand University'
         },
         {
-            title: 'Member of Computer Scientific Student\'s Community',
-            date: 'DEC 2020 - DEC 2021',
+            title: 'Content preparation for Computer Student\'s Magazine (Magit)',
+            date: 'MAR 2021 - DEC 2021',
             location: 'Ershad Damavand University'
         },
         {
@@ -27,11 +27,22 @@ function Experience() {
         <div className={style.experienceContainer}>
             <Template title='Experience' color='darkPurple' direction='rtl'>
                 <div className={style.experience}>
-                    {
-                        experienceItems.map((item, index) => (
-                            <ExperienceItem key={`experience-item-${index + 1}`} data={item}/>
-                        ))
-                    }
+                    <h3>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus fugit harum nihil
+                    </h3>
+
+                    <p>
+                        Self-motivated and creative Web Designer and Front-end Developer. Interested in learning,
+                        programming, web design, artwork and problem solving.
+                    </p>
+
+                    <div className={style.experienceItems}>
+                        {
+                            experienceItems.map((item, index) => (
+                                <ExperienceItem key={`experience-item-${index + 1}`} data={item}/>
+                            ))
+                        }
+                    </div>
                 </div>
             </Template>
 
