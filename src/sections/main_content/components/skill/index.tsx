59ff66a0f2232dd060a349083595617f74b9a8b5
Helya Moradi <helya.m78@gmail.com>
2024-04-13 11:24:59 +0330

    feat: add observer animation to skill

--- src/sections/main_content/components/skill/index.tsx
+++ src/sections/main_content/components/skill/index.tsx
@@ -1,4 +1,5 @@
 import style from './index.sass'
+import {useEffect, useRef} from "react";
 
 interface SkillProps {
     title: String;
@@ -6,10 +7,24 @@ interface SkillProps {
 }
 
 function Skill({title, percent}: SkillProps) {
+    const range = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add(style.rangeAnimation)
+                }
+            })
+        });
+
+        observer.observe(range.current)
+    }, [])
+
     return (
         <div className={style.mainContainer}>
             <div className={style.rangeContainer}>
-                <span className={style.range} style={{width:`${percent}%`}}>
+                <span className={style.range} ref={range} style={{width: `${percent}%`}}>
                     {title}
                 </span>
             </div>
