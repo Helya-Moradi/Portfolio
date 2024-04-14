c34aad868e627051eb6a946d118917b959418305
Helya Moradi <helya.m78@gmail.com>
2024-04-14 16:13:05 +0330

    feat: change type

--- src/sections/header/index.tsx
+++ src/sections/header/index.tsx
@@ -1,4 +1,3 @@
-import {useState} from 'react';
 import style from './index.sass'
 import cls from "src/utils/class_names";
 
@@ -10,29 +9,27 @@ interface HeaderProps {
 function Header({open, setOpen}: HeaderProps) {
 
     return (
-        <>
-            <header
-                className={cls(open && style.open)}>
+        <header
+            className={cls(open && style.open)}>
 
-                <span className={style.logo}>Helya Moradi</span>
+            <span className={style.logo}>Helya Moradi</span>
 
-                <div
-                    className={style.hamburgerIcon}
-                    onClick={() => {
-                        setOpen((prev: boolean) => !prev)
-                    }}>
-                    <span className={cls(style.line, style.line1)}/>
-                    <span className={cls(style.line, style.line2)}/>
-                    <span className={cls(style.line, style.line3)}/>
-                </div>
+            <div
+                className={style.hamburgerIcon}
+                onClick={() => {
+                    setOpen((prev: boolean) => !prev)
+                }}>
+                <span className={cls(style.line, style.line1)}/>
+                <span className={cls(style.line, style.line2)}/>
+                <span className={cls(style.line, style.line3)}/>
+            </div>
 
-                <div className={style.socialMedias}>
-                    <a href="https://www.linkedin.com/in/helya-moradi" target='_blank'>li.</a>
-                    <a href="https://github.com/Helya-Moradi" target='_blank'>gh.</a>
-                </div>
+            <div className={style.socialMedias}>
+                <a href="https://www.linkedin.com/in/helya-moradi" target='_blank'>li.</a>
+                <a href="https://github.com/Helya-Moradi" target='_blank'>gh.</a>
+            </div>
 
-            </header>
-        </>
+        </header>
     );
 }
 
