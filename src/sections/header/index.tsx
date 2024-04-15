9b4aa4b2494fb00e9b296d39ce4e754a6740122f
Helya Moradi <helya.m78@gmail.com>
2024-04-15 17:30:52 +0330

    feat: handle change theme

--- src/sections/header/index.tsx
+++ src/sections/header/index.tsx
@@ -10,13 +10,23 @@ interface HeaderProps {
 }
 
 function Header({open, setOpen}: HeaderProps) {
+    const changeThemeHandler = () => {
+        const body = document.body;
+
+        if (body.dataset.theme === 'light') {
+            body.removeAttribute('data-theme');
+            return
+        }
+
+        body.setAttribute('data-theme', 'light');
+    }
 
     return (
         <header>
             <span className={style.logo}>Helya Moradi</span>
 
             <div className={style.wrapper}>
-                <FontAwesomeIcon icon={faPaintBrush} className={style.icon}/>
+                <FontAwesomeIcon icon={faPaintBrush} className={style.icon} onClick={changeThemeHandler}/>
                 <HamburgerMenu open={open} setOpen={setOpen} classNames={style.hamburgerIcon}/>
             </div>
 
