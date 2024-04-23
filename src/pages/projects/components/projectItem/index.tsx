e1c57b05801ca8471567ec2c46d661023be34f09
Helya Moradi <helya.m78@gmail.com>
2024-04-23 17:16:53 +0330

    feat: add hover to projects

--- src/pages/projects/components/projectItem/index.tsx
+++ src/pages/projects/components/projectItem/index.tsx
@@ -1,15 +1,18 @@
 import style from './index.sass'
+import {MouseEventHandler} from "react";
 
 interface ProjectItemProps {
     img: string;
     link: string;
     description: string;
+    onEnter: MouseEventHandler<HTMLDivElement>;
+    onLeave: MouseEventHandler<HTMLDivElement>;
 }
 
-function ProjectItem({img, link, description}: ProjectItemProps) {
+function ProjectItem({img, link, description, onEnter, onLeave}: ProjectItemProps) {
     return (
-        <div className={style.projectItem}>
-            <a href={link} className="project" target="_blank">
+        <div className={style.projectItem} onMouseEnter={onEnter} onMouseLeave={onLeave}>
+            <a href={link} target="_blank">
                 <img src={img} alt="project-image"/>
                 <span>{description}</span>
             </a>
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,15 +1,72 @@
 import style from './index.sass'
 import ProjectItem from "src/pages/projects/components/projectItem";
 import projectsData from "src/data/projects";
+import {useEffect, useRef, useState} from "react";
 
 function Projects() {
+    const [hoverData, setHoverData]: any = useState({});
+    const [isHover, setIsHover] = useState(false);
+    const hoverDim = useRef(null);
+    const projectPage = useRef(null);
+
+    const mouseEnterHandler = async (e) => {
+        const currentProjectBound = e.currentTarget.getBoundingClientRect();
+        setHoverData(currentProjectBound)
+        addHover();
+    }
+
+    const mouseLeaveHandler = () => {
+        setHoverData({});
+        deleteHover();
+    }
+
+    const addHover = () => {
+        setIsHover(true);
+    }
+
+    const deleteHover = () => {
+        setIsHover(false);
+    }
+
+    useEffect(() => {
+        const dim = hoverDim.current;
+
+        if (isHover) {
+            const marginLeft = projectPage.current.getBoundingClientRect().x;
+            const scrollY = window.scrollY;
+
+            dim.style.top = `${hoverData.y + scrollY}px`;
+            dim.style.left = `${hoverData.x - marginLeft}px`;
+            dim.style.width = `${hoverData.width - 50}px`;
+            dim.style.height = `${hoverData.height - 50}px`;
+
+            dim.classList.add(style.hover)
+
+        } else {
+            dim.classList.remove(style.hover)
+        }
+    }, [hoverData])
+
     return (
-        <div className={style.projectsPage}>
+        <div className={style.projectsPage} ref={projectPage}>
             {
                 projectsData.map((project, index) => (
-                    <ProjectItem key={`project_item_${index + 1}`} img={project.img} link={project.link} description={project.description}/>
+                    <ProjectItem
+                        key={`project_item_${index + 1}`}
+                        img={project.img} link={project.link}
+                        description={project.description}
+                        onEnter={mouseEnterHandler}
+                        onLeave={mouseLeaveHandler}
+                    />
                 ))
             }
+
+            <span className={style.hoverDim}
+                  ref={hoverDim}
+                  onMouseEnter={addHover}
+                  onMouseMove={addHover}
+                  onMouseLeave={deleteHover}
+            />
         </div>
     );
 }
