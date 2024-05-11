9a28a9b532d143ab16679b7c45763b78a0a58a84
Helya Moradi <helya.m78@gmail.com>
2024-05-11 17:02:46 +0330

    feat: add canvas to main image

--- src/pages/main_content/sections/home/index.tsx
+++ src/pages/main_content/sections/home/index.tsx
@@ -1,11 +1,133 @@
 import style from './index.sass'
-import iImg from 'src/assets/images/i.png'
+import myImg from "src/assets/images/myImg.png"
 import ZigzagAnimation from "src/components/zigzagAnimation";
+import {useEffect, useRef, useState} from "react";
 
 function Home({observerRef}: any) {
+    const canvasRef = useRef(null);
+    const imgRef = useRef(null);
+    const [windowSize,setWindowSize] =useState(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.width = canvas.parentElement.clientWidth;
+        canvas.height = canvas.parentElement.clientHeight;
+        const image = imgRef.current;
+        image.style.border = '1px solid red'
+        const ctx = canvas.getContext("2d");
+
+        class Particle {
+            constructor(effect, x, y, color) {
+                this.effect = effect;
+                this.x = Math.random() * this.effect.width;
+                this.y = Math.random() * this.effect.height;
+                this.originX = Math.floor(x);
+                this.originY = Math.floor(y);
+                this.color = color;
+                this.size = this.effect.gap;
+                this.vx = 0;
+                this.vy = 0;
+                this.ease = 0.2;
+                this.friction = 0.9;
+                this.dx = 0;
+                this.dy = 0;
+                this.distance = 0;
+                this.force = 0;
+                this.angle = 0;
+            }
+
+            draw(context) {
+                context.fillStyle = this.color;
+                context.fillRect(this.x, this.y, this.size, this.size);
+            }
+
+            update() {
+                this.dx = this.effect.mouse.x - this.x;
+                this.dy = this.effect.mouse.y - this.y;
+                this.distance = this.dx * this.dx + this.dy * this.dy;
+                this.force = -this.effect.mouse.radius / this.distance;
+
+                if (this.distance < this.effect.mouse.radius) {
+                    this.angle = Math.atan2(this.dy, this.dx);
+                    this.vx += this.force * Math.cos(this.angle);
+                    this.vy += this.force * Math.sin(this.angle);
+                }
+
+                this.x += (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+                this.y += (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
+            }
+        }
+
+        class Effect {
+            constructor(width, height) {
+                this.width = width;
+                this.height = height;
+                this.particlesArray = [];
+                this.image = image;
+                this.gap = 2;
+                this.mouse = {
+                    radius: 3000,
+                    x: undefined,
+                    y: undefined,
+                };
+
+                let canvasRect = canvas.getBoundingClientRect();
+
+                window.addEventListener('mousemove', (e) => {
+                    this.mouse.x = e.x - canvasRect.x;
+                    this.mouse.y = e.y - canvasRect.y;
+                })
+            }
+
+            init(context) {
+                context.drawImage(this.image, 20, 40, this.width - 40, this.height - 40);
+                const pixels = context.getImageData(0, 0, this.width, this.height).data;
+                for (let y = 0; y < this.height; y += this.gap) {
+                    for (let x = 0; x < this.width; x += this.gap) {
+                        const index = (y * this.width + x) * 4;
+                        const red = pixels[index];
+                        const green = pixels[index + 1];
+                        const blue = pixels[index + 2];
+                        const alpha = pixels[index + 3];
+                        const color = `rgb(${red},${green},${blue})`;
+
+                        if (alpha > 0) {
+                            this.particlesArray.push(new Particle(this, x, y, color));
+                        }
+                    }
+                }
+            }
+
+            draw(context) {
+                this.particlesArray.forEach(particle => particle.draw(context));
+            }
+
+            update() {
+                this.particlesArray.forEach(particle => particle.update());
+            }
+        }
+
+        const effect = new Effect(canvas.width, canvas.height);
+        effect.init(ctx);
+
+        function animate() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            effect.draw(ctx);
+            effect.update();
+            requestAnimationFrame(animate);
+        }
+
+        animate();
+    }, [windowSize])
+
+    window.addEventListener('resize',()=>{
+        setWindowSize(window.innerWidth)
+    })
+
     return (<div className={style.home} id='home' ref={observerRef}>
         <div className={style.imageContainer}>
-            <img src={iImg} alt="image"/>
+            <img src={myImg} alt="image" ref={imgRef}/>
+            <canvas id={style.canvas} ref={canvasRef}/>
         </div>
 
         <div className={style.introduce}>
@@ -13,7 +135,7 @@ function Home({observerRef}: any) {
                     Hello <br/>I’m Helya Moradi
                 </span>
 
-                <span className={style.description}>
+            <span className={style.description}>
                     Self-motivated and creative web designer and front-end developer. Interested in learning, programming, web design, artwork and problem solving.
                 </span>
         </div>
